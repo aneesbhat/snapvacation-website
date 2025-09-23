53f9ace43e5c3e2fab7dc34e3d8d437b81e92627
aneesbhat <bisma1055@gmail.com>
2025-09-23 14:11:02 +0530

    changes made for performance audit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
   const preloader = document.getElementById("preloader");
   preloader.style.opacity = "0";
   preloader.style.visibility = "hidden";
-  setTimeout(() => preloader.style.display = "none", 200);
+  setTimeout(() => preloader.style.display = "none", 100);
 
   // Auto-open popup after 3s
   setTimeout(openPopup, 3000);
